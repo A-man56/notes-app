0d5786948dcf6dd5f5665b6fe7b65be3496e5887
A-man56 <144921879+A-man56@users.noreply.github.com>
2025-09-01 02:25:20 +0530

    Update BackgroundPattern.tsx

--- frontend/src/components/layout/BackgroundPattern.tsx
+++ frontend/src/components/layout/BackgroundPattern.tsx
@@ -3,7 +3,6 @@ import React from "react";
 export function BackgroundPattern() {
   return (
     <div className="w-full h-full">
-      <img
        <img src="/6931402.jpg"
         alt="Background"
         className="w-full h-full object-cover"
